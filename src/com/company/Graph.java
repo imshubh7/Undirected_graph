ab955c8b6bd7e74245fb701921fb94b7bb32bbd7
Shubham <ishubhchaudhari@gmail.com>
2020-05-13 19:46:37 +0530

    BFS added...

--- src/com/company/Graph.java
+++ src/com/company/Graph.java
@@ -6,41 +6,70 @@ public class Graph {
     ArrayList<Integer>graph[];
     int V;
 
-    public Graph(int n)
+    //GRAPH CONSTRUCTOR
+    public Graph(int v)
     {
-        this.graph=new ArrayList[n];
-        V=n;
-        for(int i=0;i<n;i++)
+        graph=new ArrayList[v];
+        for(int i=0;i<v;i++)
         {
             graph[i]=new ArrayList<>();
         }
+        V=v;
     }
 
+    // ADDING THE EDGES
     public void addEdge(int u,int v)
     {
         graph[u].add(v);
         graph[v].add(u);
     }
 
-    public void dfs(int root,boolean visited[])
-    {
-        System.out.print(root+"=>");
-        visited[root]=true;
-        for(int i=0;i<graph[root].size();i++)
-        {
-            if(!visited[graph[root].get(i)])
-            {
-                dfs(graph[root].get(i),visited);
-            }
-        }
-        return;
-    }
-
+    // DFS CALLED FROM MAIN
     public void dfs(int root)
     {
-        boolean visited[]=new boolean[V];
-        dfs(root,visited);
-        return;
+        boolean vis[]=new boolean[V];
+        System.out.println("DFS Running...");
+        dfs(root,vis);
     }
+
+    //INBUILT HELPER DFS FUNCTION WITH VISITED ARRAY
+    public void dfs(int root,boolean vis[])
+    {
+        vis[root]=true;
+        System.out.println(root);
+        for(int i=0;i<graph[root].size();i++)
+        {
+            if(!vis[graph[root].get(i)])
+            {
+                dfs(graph[root].get(i),vis);
+            }
+        }
+    }
+
+    //BFS FUNCTION USING QUEUE
+    public void bfs(int root)
+    {
+        boolean vis[]=new boolean[V];
+        System.out.println("DFS Running...");
+
+        Queue<Integer>q=new LinkedList<>();
+        ((LinkedList<Integer>) q).add(root);
+        while (q.size()>0)
+        {
+            int cur_node=q.poll();
+            System.out.println(cur_node);
+            vis[cur_node]=true;
+            for(int i=0;i<graph[cur_node].size();i++)
+            {
+                if(!vis[graph[cur_node].get(i)])
+                {
+                    ((LinkedList<Integer>) q).add(graph[cur_node].get(i));
+                }
+            }
+        }
+    }
+    //
+
+
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,8 +7,9 @@ public class Main {
         Graph graph=new Graph(5);
         graph.addEdge(0,1);
         graph.addEdge(0,2);
-        graph.addEdge(0,3);
-        graph.addEdge(1,2);
+        graph.addEdge(1,3);
+        graph.addEdge(3,4);
         graph.dfs(0);
+        graph.bfs(0);
     }
 }
